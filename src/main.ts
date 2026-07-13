fd1f7942b1f6ade7151b60c91534714f66289f92
Henri Ralph Mondoh Bebey <Henribebey.pro@gmail.com>
2026-07-13 15:37:04 +0100

    Global updates on app logic

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,3 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import { ValidationPipe } from '@nestjs/common';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   app.useGlobalPipes(new ValidationPipe({
-//     whitelist: true,       // supprime les champs non déclarés dans le DTO
-//     forbidNonWhitelisted: true, // erreur si champs inconnus envoyés
-//     transform: true,       // transforme les types automatiquement
-//   }));
-//   await app.listen(process.env.PORT ?? 3000);
-// }
-// bootstrap();
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
